dd29736cbb5cfc853920eb75e84ca142508bd6a4
radicalDilettante <wonjun92@gmail.com>
2023-01-24 21:24:18 +1300

    fix: basePath

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: process.env.NODE_ENV === "production" ? "/pdf-editor" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/pdf-editor" : "",
 };
 
 module.exports = nextConfig;
