dde516b32ae7278b9154f0adb4f3288fea066622
radicalDilettante <wonjun92@gmail.com>
2023-01-24 21:31:08 +1300

    fix: favicon

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: process.env.NODE_ENV === "production" ? "/pdf-editor" : "",
-  basePath: process.env.NODE_ENV === "production" ? "/pdf-editor" : "",
 };
 
 module.exports = nextConfig;
