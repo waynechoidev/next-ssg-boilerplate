ab0ddd792b700c5444354e311feb86d29d523dae
radicalDilettante <wonjun92@gmail.com>
2023-01-25 23:06:47 +1300

    fix: prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === "production" ? "/pdf-editor" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/next-ssg-boilerplate" : "",
   compiler: {
     styledComponents: true,
   },
